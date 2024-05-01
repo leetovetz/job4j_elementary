f7eff6bc048646eeffec35242ea67705564f2550
Serge Kravchenko <0634380033@ukr.net>
2024-05-01 18:13:51 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/Multiple.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+public class Multiple {
+    public static void main(String[] args) {
+        System.out.println("Таблица умножения на 1:");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("1 * " + i + " = " + (1 * i));
+        }
+    }
+}
