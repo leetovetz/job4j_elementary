d74219efec44f38b000972424c155e77e34fce6b
Serge Kravchenko <0634380033@ukr.net>
2024-05-18 00:23:17 +0300

    4.2. Тернарное сравнение [#189]

--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -3,12 +3,12 @@ package ru.job4j.condition;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
-class MaxiTest {
+class MaxTest {
     @Test
     void whenMax1To2Then2() {
         int left = 1;
         int right = 2;
-        int result = Maxi.max(left, right);
+        int result = Max.max(left, right);
         int expected = 2;
         assertThat(result).isEqualTo(expected);
     }
@@ -17,7 +17,7 @@ class MaxiTest {
     void whenMax3To2Then3() {
         int left = 3;
         int right = 2;
-        int result = Maxi.max(left, right);
+        int result = Max.max(left, right);
         int expected = 3;
         assertThat(result).isEqualTo(expected);
     }
@@ -26,7 +26,7 @@ class MaxiTest {
     void whenMax4To5Then5() {
         int left = 4;
         int right = 5;
-        int result = Maxi.max(left, right);
+        int result = Max.max(left, right);
         int expected = 5;
         assertThat(result).isEqualTo(expected);
     }
@@ -35,7 +35,7 @@ class MaxiTest {
     void whenMaxSameNumbersThenAny() {
         int left = 6;
         int right = 6;
-        int result = Maxi.max(left, right);
+        int result = Max.max(left, right);
         int expected = 6;
         assertThat(result).isEqualTo(expected);
     }
